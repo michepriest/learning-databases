65c7ec84a24a2efce0df05a9d1f69dbf92561679
michepriest <miche.priest@gmail.com>
2018-10-17 04:25:28 +0000

    first commit

---
+++ db-assignment/lookup_people.js
@@ -0,0 +1,27 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE first_name = $1", [process.argv[2]], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows); //output: 1
+    client.end();
+  });
+});
+
+console.log("After execution of the queries");
+
